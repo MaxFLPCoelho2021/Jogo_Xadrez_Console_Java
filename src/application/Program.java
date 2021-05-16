5ba3fb0c79d2f9cbad1d654d4d3c380e48608a61
MaxFLPCoelho2021 <max2021flpcoelho@gmail.com>
2021-05-16 01:51:47 -0300

    Movimento especial promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,13 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Entre com a peça para promoção (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+					
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
